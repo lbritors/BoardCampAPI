36db91d764552313068091b155028e83b8926c8a
lbritors <britoleticiars@gmail.com>
2023-05-15 16:30:29 -0300

    chore: adjust code for deploy on render

--- src/database/database.connection.js
+++ src/database/database.connection.js
@@ -8,5 +8,7 @@ const { Pool } = pg;
 const configDatabase = {
     connectionString: process.env.DATABASE_URL
 };
-let db;
-export default db = new Pool(configDatabase);
+
+if (process.env.MODE === "prod") configDatabase.ssl = true;
+
+export const db = new Pool(configDatabase);
